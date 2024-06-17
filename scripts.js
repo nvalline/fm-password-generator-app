0658f889255dba62104cbb62d6fd4f7db9b5cda8
vall4251n <58278138+nvalline@users.noreply.github.com>
2024-06-17 14:23:39 -0600

    added slider toast

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const passwordRangeEl = document.getElementById('passwordRange');
 const ratingEls = document.querySelectorAll('.rating');
 const ratingWrapper = document.getElementById('ratings');
 const ratingTextEL = document.getElementById('ratingText');
+const sliderToastEl = document.getElementById('sliderToast');
 const sliderValueEl = document.getElementById('sliderValue');
 
 // Style slider progress
@@ -15,6 +16,15 @@ passwordRangeEl.addEventListener('input', (event) => {
 	passwordRangeEl.style.background = `linear-gradient(to right, var(--clr-primary-green) ${progress}%, var(--clr-neutral-gray-very-dark) ${progress}%)`;
 });
 
+// Handle slide toast
+const hideSliderToast = () => {
+	sliderToastEl.className = 'toastHidden';
+};
+
+const showSliderToast = () => {
+	sliderToastEl.className = 'toastActive';
+};
+
 // Get form data to set Password Strength
 formEl.addEventListener('input', (e) => {
 	let formData = new FormData(formEl);
@@ -22,18 +32,21 @@ formEl.addEventListener('input', (e) => {
 	let entriesLength = Object.keys(entries).length;
 
 	if (entries.slider < 1) {
-		//! Add toast
-		console.log('Password Character Length cannot be 0.');
+		showSliderToast();
 	} else if (entries.slider > 0 && entriesLength <= 2) {
+		hideSliderToast();
 		ratingTextEL.innerHTML = 'Too Weak!';
 		ratingWrapper.classList = 'ratings tooWeak';
 	} else if (entries.slider > 0 && entriesLength === 3) {
+		hideSliderToast();
 		ratingTextEL.innerHTML = 'Weak';
 		ratingWrapper.classList = 'ratings weak';
 	} else if (entries.slider > 0 && entriesLength === 4) {
+		hideSliderToast();
 		ratingTextEL.innerHTML = 'Medium';
 		ratingWrapper.classList = 'ratings medium';
 	} else if (entries.slider > 0 && entriesLength === 5) {
+		hideSliderToast();
 		ratingTextEL.innerHTML = 'Strong';
 		ratingWrapper.classList = 'ratings strong';
 	}
